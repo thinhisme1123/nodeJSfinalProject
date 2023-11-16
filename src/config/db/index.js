d0d6be5e5c49461f41232764e680088fe36b8d91
LThuy <huytraceraner@gmail.com>
2023-11-16 17:57:30 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-//Không dùng localhost thay cho 127.0.0.1 được, phải là 127.0.0.1 thì mới connect được => like 
+//Không dùng localhost thay cho 127.0.0.1 được, phải là 127.0.0.1 thì mới connect được => like
+//huytraceraner là username, nên cần thay đổi lại thành username của mình, Huy123 là password để kết nối, 
+//password sẽ được cung cấp với username luôn 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/nodeJSFinalProject')
+        await mongoose.connect('mongodb+srv://huytraceraner:Huy123@cluster0.gmnwdhc.mongodb.net/nodeJSFinalProject')
         console.log("Connect successfully !")
     } catch (error) {
         console.log("Connect fail !")
